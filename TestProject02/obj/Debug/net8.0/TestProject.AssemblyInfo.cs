e0ff4c78e813312894a62d9c769a128ff6f18fe1
Harun <harunuysaljr@gmail.com>
2024-08-22 14:20:08 +0300

    test
    test

--- TestProject02/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject02/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5219c17d4179269fd53c28eedfa1dba2f5cfca47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adbce1bda001708fcc7cdda896e0ec2630592b8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
