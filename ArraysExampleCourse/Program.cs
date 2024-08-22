6f447613879ef30912fba34951c7065a90fc7c11
Harun <harunuysaljr@gmail.com>
2024-08-22 17:53:48 +0300

    test
    test

---
+++ ArraysExampleCourse/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+
+System.Console.WriteLine(fraudulentOrderIDs[2]);
