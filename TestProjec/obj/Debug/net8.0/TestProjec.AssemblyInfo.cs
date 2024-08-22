5219c17d4179269fd53c28eedfa1dba2f5cfca47
Harun <harunuysaljr@gmail.com>
2024-08-22 10:58:48 +0300

    test
    test

--- TestProjec/obj/Debug/net8.0/TestProjec.AssemblyInfo.cs
+++ TestProjec/obj/Debug/net8.0/TestProjec.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProjec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65f2e75a1a8fbf51f843a787cbed70e9ae569e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProjec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProjec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TestProject02/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject02/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65f2e75a1a8fbf51f843a787cbed70e9ae569e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
