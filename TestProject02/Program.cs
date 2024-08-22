65f2e75a1a8fbf51f843a787cbed70e9ae569e7d
Harun <harunuysaljr@gmail.com>
2024-08-22 10:30:01 +0300

    example
    example

---
+++ TestProject02/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+
+        int roll1 = rnd.Next(1, 7);
+        int roll2 = rnd.Next(1, 14);
+        int roll3 = rnd.Next(1, 5);
+
+
+        int total = roll1 + roll2 + roll3;
+
+        Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+        if(total > 15){
+            System.Console.WriteLine("Win");
+        }
+        if(total < 14){
+            System.Console.WriteLine("lose");
+        }
+ 
+   }
+}
