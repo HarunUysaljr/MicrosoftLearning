2eef48a2c1f68f4f2faffcd42fffafc7c817a283
Harun <harunuysaljr@gmail.com>
2024-09-03 17:15:45 +0300

    main
    main

--- CodeblockExample/Program.cs
+++ CodeblockExample/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool flag = true;
+if (flag)
+{
+    int value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
+}
